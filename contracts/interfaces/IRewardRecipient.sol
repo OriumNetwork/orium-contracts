5296a6a556a1a05022ace109f9fbdab340e3b3f2
Daniel Lima <38267570+karacurt@users.noreply.github.com>
2022-09-30 23:10:28 -0300

    indentation
    Co-authored-by: Ernani São Thiago <ernanirst@gmail.com>

--- contracts/interfaces/IRewardRecipient.sol
+++ contracts/interfaces/IRewardRecipient.sol
@@ -1,5 +1,5 @@
 pragma solidity 0.8.9;
 import { IERC165 } from '@openzeppelin/contracts/utils/introspection/IERC165.sol' ;
 interface IRewardRecipient is IERC165 {
-function onTokenGeneratingEvent(bytes4 event_type, address[] calldata nft_addresses, uint256[] calldata tokenIds, address[] calldata payment_address, uint256[] calldata token_amount) external virtual;
+  function onTokenGeneratingEvent(bytes4 event_type, address[] calldata nft_addresses, uint256[] calldata tokenIds, address[] calldata payment_address, uint256[] calldata token_amount) external virtual;
 }
